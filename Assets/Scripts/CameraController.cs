7f7ec7c36a720f84aab64046a9c1e2621eab5a75
Víctor Sierra Fernández <vicsierra76@gmail.com>
2020-01-27 00:42:21 +0100

    Sistema básico
    Template de movimiento isometrico sobre el que construir el resto del juego

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float offsetSpeed;
+    [SerializeField] Vector3 offset;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, offsetSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [Header("Player Variables")]
+    [SerializeField] float speed;
+    [SerializeField] float weaponDistance;
+    [SerializeField] Transform weapon;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float mouseVectorX = (Input.mousePosition.x - Screen.width / 2) - transform.position.x;
+        float mouseVectorY = (Input.mousePosition.y - Screen.height / 2) - transform.position.y;
+        Vector3 mouseVector = new Vector3(mouseVectorX, mouseVectorY, 0);
+        mouseVector.Normalize();
+
+        weapon.position = transform.position + mouseVector * weaponDistance;
+        if(mouseVectorY >= 0)
+            weapon.rotation = Quaternion.Euler(0,0, Vector3.Angle(mouseVector, Vector3.right) - 90);
+        else
+            weapon.rotation = Quaternion.Euler(0, 0, -Vector3.Angle(mouseVector, Vector3.right) - 90);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed;
+    }
+}
