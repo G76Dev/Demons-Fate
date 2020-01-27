d55f9283d9f302ccca50113d19b397e2ea107cb9
Víctor Sierra Fernández <vicsierra76@gmail.com>
2020-01-27 22:12:21 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/shootingController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingController : MonoBehaviour
+{
+
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject bulletPrefab;
+
+    [SerializeField] float bulletForce = 29f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab,firePoint.position, firePoint.rotation);
+        Rigidbody2D rb2d = bullet.GetComponent<Rigidbody2D>();
+        rb2d.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+
+    }
+}
